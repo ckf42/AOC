38d9a8fd906f5ef982c02e79b012dc00c06bf8c6
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-12-11 16:16:59 +0800

    update 2015 sol21

--- 2015/day21.py
+++ 2015/day21.py
@@ -20,8 +20,7 @@ Damage +2    50     2       0
 Damage +3   100     3       0
 Defense +1   20     0       1
 Defense +2   40     0       2
-Defense +3   80     0       3
-"""
+Defense +3   80     0       3"""
 weaponList = tuple(util.getInts(l) for l in weapons.splitlines())
 armorList = tuple(util.getInts(l) for l in armors.splitlines())
 ringList = tuple(util.getInts(l)[1:] for l in rings.splitlines())
@@ -30,39 +29,61 @@ ac = len(armorList)
 rc = len(ringList)
 baseHp = 100
 bossStat = util.getInts(util.getInput(d=21, y=2015))
-# maxDamageDeal = sum(item[1] for eqClass in (weaponList, armors, ringList) for item in eqClass)
+maxDamageDeal = sum(item[1] for eqClass in (weaponList, armorList, ringList) for item in eqClass)
+maxSpent = sum(item[0] for eqClass in (weaponList, armorList, ringList) for item in eqClass)
 
-# part 1
 model = cp_model.CpModel()
-weaponVarList = list()
-armorVarList = list()
-ringVarList = list()
-for idx in range(wc):
-    weaponVarList.append(model.NewBoolVar(f'weapon{idx}'))
-for idx in range(ac):
-    armorVarList.append(model.NewBoolVar(f'armor{idx}'))
-for idx in range(rc):
-    ringVarList.append(model.NewBoolVar(f'ring{idx}'))
+weaponVarList = tuple(model.NewBoolVar(f'weapon{idx}') for idx in range(wc))
+armorVarList = tuple(model.NewBoolVar(f'armor{idx}') for idx in range(ac))
+ringVarList = tuple(model.NewBoolVar(f'ring{idx}') for idx in range(rc))
 model.AddExactlyOne(*weaponVarList)
 model.AddAtMostOne(*armorVarList)
 model.Add(sum(ringVarList) <= 2)
 damageVar = sum(c[idx][1] * v[idx]
-                for c, v in zip((weaponList, ringList),
-                                (weaponVarList, ringVarList))
+                for c, v in zip((weaponList, ringList[:3]),
+                                (weaponVarList, ringVarList[:3]))
                 for idx in range(len(c)))
 defenceVar = sum(c[idx][2] * v[idx]
-                 for c, v in zip((armorList, ringList),
-                                 (armorVarList, ringVarList))
+                 for c, v in zip((armorList, ringList[3:]),
+                                 (armorVarList, ringVarList[3:]))
                  for idx in range(len(c)))
 costVar = sum(c[idx][0] * v[idx]
               for c, v in zip((weaponList, armorList, ringList),
                               (weaponVarList, armorVarList, ringVarList))
               for idx in range(len(c)))
-# damageDealVar = model.NewIntVar(1, maxDamageDeal, 'dam')
-# damageTakeVar = model.NewIntVar(1, bossStat[1], 'dam')
-# model.AddMaxEquality(damageDealVar)
-model.Add(baseHp * max(damageVar - bossStat[2], 1) >= bossStat[0] * max(bossStat[1] - defenceVar, 1))
+damageDealVar = model.NewIntVar(1, maxDamageDeal, 'dam')
+model.AddMaxEquality(damageDealVar, (damageVar - bossStat[2], 1))
+damageTakeVar = model.NewIntVar(1, bossStat[1], 'def')
+model.AddMaxEquality(damageTakeVar, (bossStat[1] - defenceVar, 1))
+surviveTurn = model.NewIntVar(0, baseHp, 'survive')
+model.AddDivisionEquality(surviveTurn, baseHp + damageTakeVar - 1, damageTakeVar)
+killTure = model.NewIntVar(0, bossStat[0], 'kill')
+model.AddDivisionEquality(killTure, bossStat[0] + damageDealVar - 1, damageDealVar)
+needToWin = model.NewBoolVar('toWin')
+model.Add(surviveTurn >= killTure).OnlyEnforceIf(needToWin)
+model.Add(surviveTurn < killTure).OnlyEnforceIf(needToWin.Not())
+
+# part 1
+model.AddAssumption(needToWin)
 model.Minimize(costVar)
+print(model.ModelStats())
+print(model.Validate())
+solver = cp_model.CpSolver()
+solStatus = solver.Solve(model)
+print(solver.StatusName())
+if solStatus in (cp_model.FEASIBLE, cp_model.OPTIMAL):
+    print('cost', solver.Value(costVar))
 
 # part 2
+model.ClearAssumptions()
+model.ClearObjective()
+model.AddAssumption(needToWin.Not())
+model.Maximize(costVar)
+print(model.ModelStats())
+print(model.Validate())
+solver = cp_model.CpSolver()
+solStatus = solver.Solve(model)
+print(solver.StatusName())
+if solStatus in (cp_model.FEASIBLE, cp_model.OPTIMAL):
+    print('cost', solver.Value(costVar))
 
