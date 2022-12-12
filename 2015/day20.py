59a61334e3f61122c440d5e5c835aed536d533f8
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-12-12 13:44:28 +0800

    update 2015 sol20

--- 2015/day20.py
+++ 2015/day20.py
@@ -3,20 +3,27 @@ import util
 import sympy as sp
 import itertools as it
 
-# TODO: need faster algorithm
-
 if __name__ != '__main__':
     exit()
 
-inp = '123'
+inp = '1234567'
 inp = int(inp)
 
 # part 1
+# TODO: need faster algorithm. Is it possible to avoid doing factorization?
+# TODO: try using the same method as part 2 with iteratively deepening `factorRatio`
 print(util.firstSuchThat(it.count(2),
                          lambda n: sp.divisor_sigma(n, 1) * 10 >= inp)[1])
 
 # part 2
-print(util.firstSuchThat(it.count(2),
-                         lambda n: sum(filter(lambda d: d * 50 >= n,
-                                              sp.divisors(n))) * 11 >= inp)[1])
+# print(util.firstSuchThat(it.count(2),
+                         # lambda n: sum(filter(lambda d: d * 50 >= n,
+                                              # sp.divisors(n))) * 11 >= inp)[1])
+
+factorRatio = 50
+
+def s(n):
+    return 1 + sum(n // i for i in range(2, min(n, factorRatio + 1)) if n % i == 0)
+
+print(util.firstSuchThat(it.count(2), lambda n: s(n) * 11 >= inp)[1])
 
