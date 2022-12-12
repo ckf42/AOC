08d683208ea4f88ed6ae69966d0418d19083c05b
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-12-12 19:40:52 +0800

    update 2015 sol20

--- 2015/day20.py
+++ 2015/day20.py
@@ -12,18 +12,21 @@ inp = int(inp)
 # part 1
 # TODO: need faster algorithm. Is it possible to avoid doing factorization?
 # TODO: try using the same method as part 2 with iteratively deepening `factorRatio`
-print(util.firstSuchThat(it.count(2),
-                         lambda n: sp.divisor_sigma(n, 1) * 10 >= inp)[1])
+# print(util.firstSuchThat(it.count(2),
+                         # lambda n: sp.divisor_sigma(n, 1) * 10 >= inp)[1])
 
 # part 2
 # print(util.firstSuchThat(it.count(2),
                          # lambda n: sum(filter(lambda d: d * 50 >= n,
                                               # sp.divisors(n))) * 11 >= inp)[1])
 
+# thank you @zfengg for the solution
 factorRatio = 50
 
 def s(n):
-    return 1 + sum(n // i for i in range(2, min(n, factorRatio + 1)) if n % i == 0)
+    return 1 + sum(dm[0]
+                   for i in range(2, min(n, factorRatio + 1))
+                   if (dm := divmod(n, i)) and dm[1] == 0)
 
 print(util.firstSuchThat(it.count(2), lambda n: s(n) * 11 >= inp)[1])
 
