711450a740499db2fe5e25a9f564d65145e4e92d
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-12-13 22:53:31 +0800

    update 2022 sol13

--- 2022/day13.py
+++ 2022/day13.py
@@ -7,7 +7,9 @@ if __name__ != '__main__':
     exit()
 
 packageGroup = util.getInput(d=13, y=2022).split('\n\n')
-packageList = tuple(tuple(l.splitlines()) for l in packageGroup)
+packageList = tuple(tuple(ast.parse(ps, mode='eval').body
+                          for ps in l.splitlines())
+                    for l in packageGroup)
 
 def isInOrder(a, b):
     if isinstance(a, ast.Constant) and isinstance(b, ast.Constant):
@@ -15,7 +17,7 @@ def isInOrder(a, b):
     elif isinstance(a, ast.Constant):
         return isInOrder(ast.parse('[' + ast.unparse(a) + ']', mode='eval').body, b)
     elif isinstance(b, ast.Constant):
-        return isInOrder(a, ast.parse('[' + ast.unparse(b) + ']', mode='eval').body)
+        return -isInOrder(b, a)
     else:
         la = len(a.elts)
         lb = len(b.elts)
@@ -26,17 +28,15 @@ def isInOrder(a, b):
         return 0 if la == lb else (-1 if la < lb else 1)
 
 # part 1
-counter = 0
-for idx in range(len(packageList)):
-    if isInOrder(ast.parse(packageList[idx][0], mode='eval').body,
-                 ast.parse(packageList[idx][1], mode='eval').body) == -1:
-        counter += 1 + idx
-print(counter)
+print(sum(i + 1
+          for i in range(len(packageList))
+          if isInOrder(*packageList[i]) == -1))
 
 # part 2
-packages = tuple((pl, False) for gp in packageList for pl in gp) \
-        + tuple(zip(('[[2]]', '[[6]]'), (True, True)))
-packageStream = list((ast.parse(p, mode='eval').body, v) for (p, v) in packages)
+packageStream = list((pl, False) for gp in packageList for pl in gp) \
+        + list(zip(tuple(ast.parse(s, mode='eval').body
+                         for s in ('[[2]]', '[[6]]')),
+                   (True, True)))
 packageStream.sort(key=lambda pv: cmp_to_key(isInOrder)(pv[0]))
 print(util.prod(i + 1 for i, pv in enumerate(packageStream) if pv[1]))
 
