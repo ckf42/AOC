b7d6cfa2a2021d28f90a99ded8e5e0568794ad6a
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-12-13 13:52:20 +0800

    add 2022 sol13

---
+++ 2022/day13.py
@@ -0,0 +1,42 @@
+import AOCInit
+import util
+import ast
+from functools import cmp_to_key
+
+if __name__ != '__main__':
+    exit()
+
+packageGroup = util.getInput(d=13, y=2022).split('\n\n')
+packageList = tuple(tuple(l.splitlines()) for l in packageGroup)
+
+def isInOrder(a, b):
+    if isinstance(a, ast.Constant) and isinstance(b, ast.Constant):
+        return (0 if a.value == b.value else (-1 if a.value < b.value else 1))
+    elif isinstance(a, ast.Constant):
+        return isInOrder(ast.parse('[' + ast.unparse(a) + ']', mode='eval').body, b)
+    elif isinstance(b, ast.Constant):
+        return isInOrder(a, ast.parse('[' + ast.unparse(b) + ']', mode='eval').body)
+    else:
+        la = len(a.elts)
+        lb = len(b.elts)
+        for idx in range(min(la, lb)):
+            c = isInOrder(a.elts[idx], b.elts[idx])
+            if c != 0:
+                return c
+        return 0 if la == lb else (-1 if la < lb else 1)
+
+# part 1
+counter = 0
+for idx in range(len(packageList)):
+    if isInOrder(ast.parse(packageList[idx][0], mode='eval').body,
+                 ast.parse(packageList[idx][1], mode='eval').body) == -1:
+        counter += 1 + idx
+print(counter)
+
+# part 2
+packages = tuple((pl, False) for gp in packageList for pl in gp) \
+        + tuple(zip(('[[2]]', '[[6]]'), (True, True)))
+packageStream = list((ast.parse(p, mode='eval').body, v) for (p, v) in packages)
+packageStream.sort(key=lambda pv: cmp_to_key(isInOrder)(pv[0]))
+print(util.prod(i + 1 for i, pv in enumerate(packageStream) if pv[1]))
+
