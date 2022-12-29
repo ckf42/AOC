23a00b6e8317f9e57ae688164cebbb332af161a6
KF Chan <31157514+ckf42@users.noreply.github.com>
2022-12-29 09:35:24 +0800

    add 2016 sol16

--- 2016/day16.py
+++ 2016/day16.py
@@ -4,26 +4,41 @@ import util
 if __name__ != '__main__':
     exit()
 
+# TODO: go with u/askalski's algorithm
+
 inp = util.getInput(d=16, y=2016).strip()
-subTuple = tuple(map(tuple, ('00', '11', '10', '01')))
+initLen = len(inp)
+initState = list(c == '1' for c in inp)
+revNegInitState = list(not c for c in initState[::-1])
 
-def step(s):
-    return s + '0' + util.subChar('01', '10', s[::-1])
+segJoiner = [False]
 
-def checksum(s):
-    while not (len(s) & 1):
-        s = ''.join(util.sub(subTuple, '1100', util.splitIntoGp(s, 2)))
-    return s
+def ensureJoinerLen(l):
+    while len(segJoiner) < l:
+        segJoiner.extend([False] + [not c for c in segJoiner[::-1]])
+
+def getChecksum(targetLen):
+    ensureJoinerLen((targetLen + 1) // (initLen + 1))
+    chunkSize = targetLen & -targetLen
+    checkSumStream = list()
+    remainParts = list()
+    joinerIdx = 0
+    useOgInp = True
+    for _ in range(targetLen // chunkSize):
+        while len(remainParts) < chunkSize:
+            remainParts += (initState if useOgInp else revNegInitState) \
+                    + segJoiner[joinerIdx:joinerIdx + 1]
+            joinerIdx += 1
+            useOgInp = not useOgInp
+        checkSumStream.append(sum(remainParts[:chunkSize]) & 1 == 0)
+        remainParts = remainParts[chunkSize:]
+    return ''.join(util.sub((True, False), '10', checkSumStream))
+
 
 # part 1
-targetLen = 272
-s = inp
-while len(s) < targetLen:
-    s = step(s)
-print(checksum(s[:targetLen]))
-
+print(getChecksum(272))
 
 # part 2
-targetLen = 35651584
+print(getChecksum(35651584))
 
 
