77fd95f002d9420f103dd5bcec55d06fbbd7b50e
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-01-01 18:36:27 +0800

    update 2016 partial sol 22

--- 2016/day22.py
+++ 2016/day22.py
@@ -1,5 +1,6 @@
 import AOCInit
 import util
+from collections import namedtuple
 from bisect import bisect_right
 
 if __name__ != '__main__':
@@ -7,54 +8,45 @@ if __name__ != '__main__':
 
 inp = util.getInput(d=22, y=2016)
 
-# pt, used, avail
-nodes = tuple((util.Point(il[0], il[1]), il[3], il[4])
-              for l in inp.strip().splitlines()[2:]
-              if (il := util.getInts(l)))
+Disk = namedtuple('Disk', ['total', 'used', 'avail'])
+# pt: used, avail
+nodes = {util.Point(il[0], il[1]): Disk(il[2], il[3], il[4])
+         for l in inp.strip().splitlines()[2:]
+         if (il := util.getInts(l))}
 nCount = len(nodes)
 
 # part 1
-usedSpace = sorted(tuple(nodes[i][1]
-                         for i in range(len(nodes))
-                         if nodes[i][1] != 0))
-availSpace = sorted(tuple(nodes[i][2]
-                          for i in range(len(nodes))),
+usedSpace = sorted(tuple(ua.used
+                         for ua in nodes.values()
+                         if ua.used != 0))
+availSpace = sorted(tuple(ua.avail
+                          for ua in nodes.values()),
                     reverse=True)
 viaCount = 0
 for avail in availSpace:
     if avail < usedSpace[0]:
         break
     viaCount += bisect_right(usedSpace, avail)
-viaCount -= util.count(nodes, lambda n: 0 != n[1] <= n[2])
+viaCount -= util.count(nodes.values(), lambda n: 0 != n.used <= n.avail)
 
 print(viaCount)
 
 # part 2
-# for a in range(nCount):
-    # for b in range(nCount):
-        # if a != b and nodes[a][1] != 0 and nodes[a][1] <= nodes[b][2]:
-            # print(nodes[a], nodes[b])
-# b always seems to be an empty disk
-# layoutDim = util.Point(*map(max, util.takeApart(tuple(n[0] for n in nodes))))
-# emptyDisks = tuple(n[0] for n in nodes if n[1] == 0)
-# directions = tuple(util.Point(*d) for d in util.integerLattice(2, 1))
-# assert len(emptyDisks) == 1
-# targetLoc = util.Point(layoutDim[0], 0)
+# for viable pair, b always seems to be an empty disk
+coorMax = util.Point(*map(max, util.takeApart(tuple(nodes.keys()))))
+emptyDisks = tuple(pt for pt, ua in nodes.items() if ua.used == 0)
+assert len(emptyDisks) == 1
+emptyDiskLoc = emptyDisks[0]
+targetDataLoc = util.Point(coorMax[0], 0)
 
-# def getNei(st):
+directions = tuple(util.Point(*d) for d in util.integerLattice(2, 1))
+
+# def getNei(state):
     # resList = list()
-    # (blankPt, dataPt) = st
+    # (emptyLoc, dataLoc) = state
     # for d in directions:
-        # newPt = blankPt + d
-        # if not (0 <= newPt < layoutDim) or :
+        # newPt = emptyLoc + d
+        # if not (0 <= newPt <= coorMax):
             # continue
-
     # return resList
-
-
-# # blank pt, target loc
-# print(util.dijkstra((emptyDisks[0], targetLoc),
-                    # lambda nst, ost, oc: oc + 1,
-                    # getNei,
-                    # lambda st: st[1] == util.Point(0, 0)))
 
