a1d0a9d9444be65b32d0ef253ed43e67fcc6d080
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-01-10 23:19:12 +0800

    add inc/dec-loop-assign optimization for 2016 sol12

--- 2016/day12.py
+++ 2016/day12.py
@@ -14,20 +14,41 @@ instDict = {i: tuple((int(term) if term[-1].isdigit() else term)
 
 # part 1
 register = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+
+# optimize instructions
+for i in range(1, l - 1):
+    if instDict[i][0] == 'dec' \
+            and instDict[i + 1] == ('jnz', instDict[i][1], -2) \
+            and instDict[i - 1][0] in ('inc', 'dec') \
+            and instDict[i - 1][1] != instDict[i][1]:
+        instDict[i - 1] = (
+                '+=' if instDict[i - 1][0] == 'inc' else '-=',
+                instDict[i - 1][1],
+                instDict[i][1]
+        )
+        instDict[i] = instDict[i + 1] = ('nop',)
+
 pid = 0
 while pid < l:
     inst = instDict[pid]
+    pidOffset = 1
     if inst[0] == 'cpy':
         register[inst[2]] = register.get(inst[1], inst[1])
-        pid += 1
     elif inst[0] == 'inc':
         register[inst[1]] += 1
-        pid += 1
     elif inst[0] == 'dec':
         register[inst[1]] -= 1
-        pid += 1
-    else:
-        pid += 1 if register.get(inst[1], inst[1]) == 0 else inst[2]
+    elif inst[0] == 'jnz':
+        if register.get(inst[1], inst[1]) != 0:
+            pidOffset = inst[2]
+            assert instDict[pid + pidOffset][0] != 'nop'
+    elif inst[0] == '+=':
+        register[inst[1]] += register[inst[2]]
+    elif inst[0] == '-=':
+        register[inst[1]] -= register[inst[2]]
+    elif inst[0] == 'nop':
+        pass
+    pid += pidOffset
 print(register['a'])
 
 # part 2
@@ -35,17 +56,24 @@ register = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
 pid = 0
 while pid < l:
     inst = instDict[pid]
+    pidOffset = 1
     if inst[0] == 'cpy':
         register[inst[2]] = register.get(inst[1], inst[1])
-        pid += 1
     elif inst[0] == 'inc':
         register[inst[1]] += 1
-        pid += 1
     elif inst[0] == 'dec':
         register[inst[1]] -= 1
-        pid += 1
-    else:
-        pid += 1 if register.get(inst[1], inst[1]) == 0 else inst[2]
+    elif inst[0] == 'jnz':
+        if register.get(inst[1], inst[1]) != 0:
+            pidOffset = inst[2]
+            assert instDict[pid + pidOffset][0] != 'nop'
+    elif inst[0] == '+=':
+        register[inst[1]] += register[inst[2]]
+    elif inst[0] == '-=':
+        register[inst[1]] -= register[inst[2]]
+    elif inst[0] == 'nop':
+        pass
+    pid += pidOffset
 print(register['a'])
 
 
