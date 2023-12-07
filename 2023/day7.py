2b5a2d63652fd27c4c70d06aecafa2dc6259efa2
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-12-07 21:41:56 +0800

    update 2023 sol 7

--- 2023/day7.py
+++ 2023/day7.py
@@ -1,57 +1,45 @@
 import AOCInit
 import util
-from functools import cmp_to_key
 from collections import Counter
 
 if __name__ != '__main__':
     exit()
 
 inp = util.getInput(d=7, y=2023)
-hands = tuple(
+hands, handPts = util.transpose(tuple(
         (line.split()[0], int(line.split()[1]))
-        for line in inp.splitlines())
+        for line in inp.splitlines()))
 
 def rankedOrdering(handList: tuple[str, ...], isPart2=False) -> list[int]:
-    cardOrdering: str = 'AKQT98765432J' if isPart2 else 'AKQJT98765432'  # small = strong
-    jRk: int = cardOrdering.index('J')
-    handOrdList: tuple[tuple[int, ...], ...] = tuple(
-            tuple(cardOrdering.index(c) for c in h)
-            for h in handList)
+    cardOrder: str = 'AKQT98765432J' if isPart2 else 'AKQJT98765432'  # small = strong
 
-    def getType(hand: tuple[int, ...]) -> int:
-        c: Counter[int] = Counter(hand)
-        if isPart2 and jRk in c and len(c) != 1:
-            cardCountOrder: list[int] = sorted(
-                    tuple(c.keys()),
-                    key=lambda k: c[k],
-                    reverse=True)
-            target: int | None = util.firstSuchThat(
-                    cardCountOrder,
-                    lambda card: card != jRk)[1]
+    def getType(hand: str) -> int:
+        c: Counter[str] = Counter(hand)
+        if isPart2 and len(c) != 1 and 'J' in c:
+            target: str | None = util.firstSuchThat(
+                    (pr[0] for pr in c.most_common()),
+                    lambda card: card != 'J')[1]
             assert target is not None
-            c[target] += c[jRk]
-            del c[jRk]
+            c[target] += c['J']
+            del c['J']
         m = max(c.values())
         return 10 * len(c) + 5 - m
 
-    def handCmp(h1: tuple[int, ...], h2: tuple[int, ...]) -> int:
-        t1, t2 = getType(h1), getType(h2)
-        if t1 == t2:
-            return 1 if h1 <= h2 else -1
-        return 1 if t1 <= t2 else -1
+    rankedIndices = sorted(
+            util.rangeLen(handList),
+            key=lambda hidx: tuple(cardOrder.index(c) for c in handList[hidx]),
+            reverse=True)
+    rankedIndices.sort(key=lambda hidx: getType(handList[hidx]), reverse=True)
+    return rankedIndices
 
-    return sorted(util.rangeLen(handOrdList),
-                  key=cmp_to_key(lambda i, j: handCmp(handOrdList[i], handOrdList[j])))
 
 # part 1
-print(sum((rk + 1) * hands[hidx][1]
+print(sum((rk + 1) * handPts[hidx]
           for rk, hidx in enumerate(
-              rankedOrdering(tuple(h[0] for h in hands),
-                             isPart2=False))))
+              rankedOrdering(hands, isPart2=False))))
 
 # part 2
-print(sum((rk + 1) * hands[hidx][1]
+print(sum((rk + 1) * handPts[hidx]
           for rk, hidx in enumerate(
-              rankedOrdering(tuple(h[0] for h in hands),
-                             isPart2=True))))
+              rankedOrdering(hands, isPart2=True))))
 
