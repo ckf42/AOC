07ce537c430209c75b0c0b4ce1afee629c93853c
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-12-09 19:34:13 +0800

    update 2023 sol 9

--- 2023/day9.py
+++ 2023/day9.py
@@ -6,36 +6,23 @@ if __name__ != '__main__':
     exit()
 
 inp = util.getInput(d=9, y=2023)
-
 histories = tuple(util.getInts(line) for line in inp.splitlines())
 
-def getSeeds(seq):
+# part 1
+# math sol probably gives blown up coeff in the polynomial
+def getPrevTerm(seq):
     seeds = list()
     while any(x != 0 for x in seq):
         seeds.append(seq[0])
         seq = util.diff(seq)
-    return seeds
-historySeeds = tuple(getSeeds(h) for h in histories)
-
-# part 1
-# math sol probably gives blown up coeff in the polynomial
-def getNextTerm(seeds, n):
-    seq = (seeds[-1],) * (n - len(seeds) + 2)
-    for s in seeds[-2::-1]:
-        seq = tuple(accumulate(seq, initial=s))
-    return seq[-1]
-
-print(sum(getNextTerm(s, l)
-          for s, l in zip(historySeeds,
-                          (len(h) for h in histories))))
-
-# part 2
-def getPrevTerm(seeds):
     head = 0
     for s in seeds[::-1]:
         head = s - head
     return head
 
-print(sum(getPrevTerm(s) for s in historySeeds))
+print(sum(getPrevTerm(seq[::-1]) for seq in histories))
+
+# part 2
+print(sum(getPrevTerm(seq) for seq in histories))
 
 
