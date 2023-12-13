00fbb673aafbfc3246f06fba41e0d16e31a5ef4c
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-12-13 13:57:26 +0800

    update 2023 sol 11

--- 2023/day11.py
+++ 2023/day11.py
@@ -6,9 +6,16 @@ if __name__ != '__main__':
     exit()
 
 inp = util.getInput(d=11, y=2023)
+# updated to work on more complicated input
+# from https://old.reddit.com/r/adventofcode/comments/18h1hho/2023_day_11_a_more_challenging_input_complexity/
+# inp = ''
+# with open('test11.txt', 'rt') as f:
+#     inp = f.read()
 
 sky = inp.splitlines()
 dim = (len(sky), len(sky[0]))
+
+# can we optimize this from 3 passes to 1 pass?
 emptyRows = tuple(
         i
         for i in range(dim[0])
@@ -17,28 +24,29 @@ emptyCols = tuple(
         j
         for j in range(dim[1])
         if all(sky[i][j] == '.' for i in range(dim[0])))
-galaxies = tuple(
-        divmod(i, dim[1] + 1)
-        for i in util.rangeLen(inp)
-        if inp[i] == '#')
-emptyCoors = tuple(
-        (bisect(emptyRows, g[0]), bisect(emptyCols, g[1]))
-        for g in galaxies)
+galaxyX = list()
+galaxyY = list()
+for i in range(dim[0]):
+    for j in range(dim[1]):
+        if sky[i][j] == '#':
+            galaxyX.append(i)
+            galaxyY.append(j)
+
+galaxyY.sort()  # only y is not inserted in order
+
+# this can be further optimized
+# by replacing bisect (n log n) with rolling (n)
+emptyX = tuple(bisect(emptyRows, x) for x in galaxyX)
+emptyY = tuple(bisect(emptyCols, y) for y in galaxyY)
+
+normalDist = sum((x + y) * (2 * i - len(galaxyX) + 1)
+                 for i, (x, y) in enumerate(zip(galaxyX, galaxyY)))
+expandDist = sum((x + y) * (2 * i - len(emptyX) + 1)
+                 for i, (x, y) in enumerate(zip(emptyX, emptyY)))
 
 # part 1
-def dist(i, j, weight=2):
-    return abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1]) \
-            + (abs(emptyCoors[i][0] - emptyCoors[j][0]) \
-            + abs(emptyCoors[i][1] - emptyCoors[j][1])) * (weight - 1)
-
-print(sum(dist(i, j)
-          for i in range(len(galaxies))
-          for j in range(i + 1, len(galaxies))))
-
+print(normalDist + expandDist)
 
 # part 2
-print(sum(dist(i, j, 1000000)
-          for i in range(len(galaxies))
-          for j in range(i + 1, len(galaxies))))
-
+print(normalDist + expandDist * (1000000 - 1))
 
