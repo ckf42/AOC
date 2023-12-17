5cbd4b5db821c0b638761cabf52d9b810642a973
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-12-17 22:36:54 +0800

    update 2023 sol 17

--- 2023/day17.py
+++ 2023/day17.py
@@ -2,7 +2,7 @@ import AOCInit
 import util
 import heapq as hq
 
-# TODO: speed up
+# TODO: speed up. Currently takes ~4s for both parts
 
 if __name__ != '__main__':
     exit()
@@ -15,52 +15,31 @@ dim = (len(heatmap), len(heatmap[0]))
 dirList = ((1, 0), (0, 1), (0, -1), (-1, 0))
 
 # part 1
-visited = set()
-h = [(0, (0, 0), -1)]  # cost, pos, prev dir idx
-while len(h) != 0:
-    cost, pos, prevDIdx = hq.heappop(h)
-    if (pos, prevDIdx) in visited:
-        continue
-    if pos == (dim[0] - 1, dim[1] - 1):
-        print(cost)
-        break
-    visited.add((pos, prevDIdx))
-    for dIdx in range(4):
-        if dIdx in (prevDIdx, 3 - prevDIdx):
+def minLoss(maxStep, minStep=1):
+    visited = set()
+    h = [(0, (0, 0), -1)]  # cost, pos, prev dir idx
+    while len(h) != 0:
+        cost, pos, prevDIdx = hq.heappop(h)
+        if (pos, prevDIdx) in visited:
             continue
-        d = dirList[dIdx]
-        newCost = cost
-        newPos = pos
-        for _ in range(1, 3 + 1):
-            newPos = (newPos[0] + d[0], newPos[1] + d[1])
-            if not (0 <= newPos[0] < dim[0] and 0 <= newPos[1] < dim[1]):
-                break
-            newCost += heatmap[newPos[0]][newPos[1]]
-            if (newPos, dIdx) not in visited:
-                hq.heappush(h, (newCost, newPos, dIdx))
+        if pos == (dim[0] - 1, dim[1] - 1):
+            return cost
+        visited.add((pos, prevDIdx))
+        for dIdx in range(4):
+            if dIdx in (prevDIdx, 3 - prevDIdx):
+                continue
+            d = dirList[dIdx]
+            newCost = cost
+            newPos = pos
+            for step in range(1, maxStep + 1):
+                newPos = (newPos[0] + d[0], newPos[1] + d[1])
+                if not (0 <= newPos[0] < dim[0] and 0 <= newPos[1] < dim[1]):
+                    break
+                newCost += heatmap[newPos[0]][newPos[1]]
+                if step >= minStep and (newPos, dIdx) not in visited:
+                    hq.heappush(h, (newCost, newPos, dIdx))
+
+print(minLoss(maxStep=3))
 
 # part 2
-visited = set()
-h = [(0, (0, 0), -1)]  # cost, pos, prev dir idx
-while len(h) != 0:
-    cost, pos, prevDIdx = hq.heappop(h)
-    if (pos, prevDIdx) in visited:
-        continue
-    if pos == (dim[0] - 1, dim[1] - 1):
-        print(cost)
-        break
-    visited.add((pos, prevDIdx))
-    for dIdx in range(4):
-        if dIdx in (prevDIdx, 3 - prevDIdx):
-            continue
-        d = dirList[dIdx]
-        newCost = cost
-        newPos = pos
-        for step in range(1, 10 + 1):
-            newPos = (newPos[0] + d[0], newPos[1] + d[1])
-            if not (0 <= newPos[0] < dim[0] and 0 <= newPos[1] < dim[1]):
-                break
-            newCost += heatmap[newPos[0]][newPos[1]]
-            if step >= 4 and (newPos, dIdx) not in visited:
-                hq.heappush(h, (newCost, newPos, dIdx))
-
+print(minLoss(maxStep=10, minStep=4))
