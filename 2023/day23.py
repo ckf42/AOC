0ddfb7379019632df9d00d02fe296f443b7bc36f
KF Chan <31157514+ckf42@users.noreply.github.com>
2023-12-24 16:59:51 +0800

    add 2023 sol 24, update sol 23

--- 2023/day23.py
+++ 2023/day23.py
@@ -30,12 +30,12 @@ nodePos = frozenset(
                         for nb in util.nearby2DGridPts((i, j), dim)) != 2)
 
 nodeLinks: dict[tuple[int, int], dict[tuple[int, int], int]] \
-        = {n: dict() for n in nodePos}
+        = {n: dict() for n in nodePos}  # source -> dest -> step
 for root in nodePos:
     linkStack = [(root, root, 0)]
     while len(linkStack) != 0:
         currPos, prevPos, step = linkStack.pop()
-        if currPos in nodePos and currPos != prevPos and currPos != root:
+        if currPos in nodePos and prevPos != currPos != root:
             nodeLinks[root][currPos] = max(nodeLinks[root].get(currPos, 0), step)
             continue
         sym = trailMap[currPos[0]][currPos[1]]
@@ -53,17 +53,22 @@ for root in nodePos:
                 continue
             linkStack.append((nb, currPos, step + 1))
 
+assert len(nodePos) < 64  # so that we do use too much mem
+nodeToMask = {n: (1 << i) for i, n in enumerate(nodePos)}
+
 # part 1
-# assume DAG
+# also work if we assume DAG (does not use visited)
+# performance does not seem affected much
 maxDist = 0
-stack = [(0, startPos)]
+stack = [(0, startPos, 0)]
 while len(stack) != 0:
-    cost, node = stack.pop()
+    cost, node, visited = stack.pop()
     if node == targetPos:
         maxDist = max(maxDist, cost)
         continue
     for nb, c in nodeLinks[node].items():
-        stack.append((cost + c, nb))
+        if not (visited & nodeToMask[nb]):
+            stack.append((cost + c, nb, visited | nodeToMask[nb]))
 print(maxDist)
 
 # part 2
@@ -75,20 +80,18 @@ for s in nodePos:
         nodeLinks[s][e] = w
         nodeLinks[e][s] = w
 
-# this takes ~1min
+# this takes ~40s
 # longest path is NP-hard unless graph has good structure
 # TODO: optimize?
 maxDist = 0
-part2Stack: list[tuple[int,
-                       tuple[int, int],
-                       set[tuple[int, int]]]] = [(0, startPos, set())]
-while len(part2Stack) != 0:
-    cost, node, visited = part2Stack.pop()
+stack = [(0, startPos, 0)]
+while len(stack) != 0:
+    cost, node, visited = stack.pop()
     if node == targetPos:
         maxDist = max(maxDist, cost)
         continue
     for nb, c in nodeLinks[node].items():
-        if nb not in visited:
-            part2Stack.append((cost + c, nb, visited.union((nb,))))
+        if not (visited & nodeToMask[nb]):
+            stack.append((cost + c, nb, visited | nodeToMask[nb]))
 print(maxDist)
 
---
+++ 2023/day24.py
@@ -0,0 +1,52 @@
+import AOCInit
+import util
+import sympy as sp
+
+if __name__ != '__main__':
+    exit()
+
+inp = util.getInput(d=24, y=2023)
+
+stones = tuple(
+        util.splitIntoGp(util.getInts(line), 3)
+        for line in inp.splitlines())
+stoneCount = len(stones)
+
+# part 1
+detectDom = (200000000000000, 400000000000000)
+
+# FIXME: util.segmentIntersection does not seem to work. Check again
+
+crosses = 0
+for i in range(stoneCount):
+    ap, av = stones[i]
+    for j in range(i + 1, stoneCount):
+        bp, bv = stones[j]
+        det = av[1] * bv[0] - av[0] * bv[1]
+        if det == 0:
+            continue
+        t = (-bv[1] * (bp[0] - ap[0]) + bv[0] * (bp[1] - ap[1])) / det
+        s = (-av[1] * (bp[0] - ap[0]) + av[0] * (bp[1] - ap[1])) / det
+        if t >= 0 and s >= 0 \
+                and detectDom[0] <= ap[0] + t * av[0] <= detectDom[1] \
+                and detectDom[0] <= ap[1] + t * av[1] <= detectDom[1]:
+            crosses += 1
+print(crosses)
+
+
+# part 2
+# TODO: check if first 3 velocities parallel. Here it just works(TM)
+# TODO: solution without sympy
+p = sp.var('x y z')
+v = sp.var('vx vy vz')
+t = sp.var('t1 t2 t3')
+equs = list()
+for i in range(3):
+    ps, vs = stones[i]
+    for d in range(3):
+        equs.append(p[d] - ps[d] + t[i] * (v[d] - vs[d]))
+res = sp.solve(equs, *p, *v, *t, dict=True)
+assert len(res) == 1, "Too many solutions"  # TODO: how to filter?
+print(sum(res[0][var] for var in p))
+
+
