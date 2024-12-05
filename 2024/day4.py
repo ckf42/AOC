f57c292b61d0ef6c279c1cef03033aa0f70666f3
KF Chan <31157514+ckf42@users.noreply.github.com>
2024-12-05 23:51:11 +0800

    update 2024 sol 4

--- 2024/day4.py
+++ 2024/day4.py
@@ -7,32 +7,32 @@ if __name__ != '__main__':
 inp = util.getInput(d=4, y=2024)
 
 lines = inp.splitlines()
-n = len(lines)
-m = len(lines[0])  # assume rect
+patt = 'XMAS'
 
 # part 1
 count = sum(
     len(util.findPattIn2DStr(lines, pattGp))
     for pattGp in (
-        'XMAS', 'XMAS'[::-1],
-        ('XMAS',), ('XMAS'[::-1],),
-        ('X', '.M', '..A', '...S'),
-        ('X', '.M', '..A', '...S')[::-1],
-        ('S', '.A', '..M', '...X'),
-        ('S', '.A', '..M', '...X')[::-1],
+        patt, patt[::-1],
+        (patt,), (patt[::-1],),
+        tuple('.' * i + c for i, c in enumerate(patt)),
+        tuple('.' * i + c for i, c in enumerate(patt))[::-1],
+        tuple('.' * i + c for i, c in enumerate(patt[::-1])),
+        tuple('.' * i + c for i, c in enumerate(patt[::-1]))[::-1],
     )
 )
 print(count)
 
 
 # part 2
+newPatt = ('M.S', '.A.', 'M.S')
 count = sum(
     len(util.findPattIn2DStr(lines, pattGp))
     for pattGp in (
-        ('M.S', '.A.', 'M.S'),
-        tuple(r[::-1] for r in ('M.S', '.A.', 'M.S')),
-        ('S.S', '.A.', 'M.M'),
-        ('S.S', '.A.', 'M.M')[::-1]
+        newPatt,
+        tuple(''.join(r[::-1]) for r in newPatt),
+        tuple(''.join(r) for r in util.transpose(newPatt)),
+        tuple(''.join(r) for r in util.transpose(newPatt))[::-1],
     )
 )
 print(count)
