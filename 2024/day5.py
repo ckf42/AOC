a9969f64c035873ab36309e87179d4f23562231c
KF Chan <31157514+ckf42@users.noreply.github.com>
2024-12-05 16:16:46 +0800

    add 2024 sol 5

---
+++ 2024/day5.py
@@ -0,0 +1,47 @@
+import AOCInit
+import util
+
+from collections import defaultdict
+from functools import cmp_to_key
+
+if __name__ != '__main__':
+    exit()
+
+inp = util.getInput(d=5, y=2024)
+
+# part 1
+rulesText, sectOrderText = inp.split('\n\n')
+rules = tuple(util.getInts(line) for line in rulesText.splitlines())
+sects = tuple(util.getInts(line) for line in sectOrderText.splitlines())
+
+edges: defaultdict[int, set[int]] = defaultdict(set)
+for a, b in rules:
+    edges[a].add(b)
+
+corrOrder = [False] * len(sects)
+def isCorrOrder(sectOrder: tuple[int, ...]) -> bool:
+    n = len(sectOrder)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if sectOrder[i] in edges[sectOrder[j]]:
+                return False
+    return True
+
+total = 0
+for i, sect in enumerate(sects):
+    corrOrder[i] = isCorrOrder(sect)
+    if corrOrder[i]:
+        total += sect[len(sect) // 2]
+print(total)
+
+# part 2
+total = 0
+for i, sect in enumerate(sects):
+    if not corrOrder[i]:
+        newOrder = sorted(
+            sect,
+            key=cmp_to_key(lambda a, b: -1 if b in edges[a] else 0)
+        )
+        total += newOrder[len(newOrder) // 2]
+print(total)
+
