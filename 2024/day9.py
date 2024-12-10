9e1e31fd1caff2dfa81645585adc00cc223247f3
KF Chan <31157514+ckf42@users.noreply.github.com>
2024-12-10 10:21:38 +0800

    update 2024 sol 9
    using idea from https://redd.it/1hab624

--- 2024/day9.py
+++ 2024/day9.py
@@ -1,6 +1,8 @@
 import AOCInit
 import util
 
+import heapq as hq
+
 if __name__ != '__main__':
     exit()
 
@@ -37,16 +39,30 @@ blocks = list(int(c) for c in inp)
 checksum = 0
 offsets = [0] * n
 currOffset = 0
+locs = [list() for _ in range(10)]
 for i in range(n):
     if (i ^ 1) & 1:
         checksum += (i // 2) * util.rangeSum(range(currOffset, currOffset + blocks[i]))
+    elif blocks[i] != 0:
+        locs[blocks[i]].append(i)
     offsets[i] = currOffset
     currOffset += blocks[i]
+for i in range(1, 10):
+    hq.heapify(locs[i])
 for i in range(n - 1, 1, -2):
-    idx = util.firstIdxSuchThat(blocks, lambda x: x >= blocks[i], s=1, e=i, step=2)
-    if idx is not None:
-        checksum -= (i // 2) * blocks[i] * (offsets[i] - offsets[idx])
-        blocks[idx] -= blocks[i]
-        offsets[idx] += blocks[i]
+    rightBlankSize = -1
+    for blankSize in range(blocks[i], 10):
+        if len(locs[blankSize]) != 0 and locs[blankSize][0] > i:
+            locs[blankSize].clear()
+        if len(locs[blankSize]) != 0:
+            if rightBlankSize == -1 or locs[blankSize][0] < locs[rightBlankSize][0]:
+                rightBlankSize = blankSize
+    if rightBlankSize != -1:
+        blankIdx = hq.heappop(locs[rightBlankSize])
+        checksum -= (i // 2) * blocks[i] * (offsets[i] - offsets[blankIdx])
+        blocks[blankIdx] -= blocks[i]
+        offsets[blankIdx] += blocks[i]
+        if blocks[blankIdx] != 0:
+            hq.heappush(locs[blocks[blankIdx]], blankIdx)
 print(checksum)
 
