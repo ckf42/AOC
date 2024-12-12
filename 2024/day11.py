0ee501cd3686426be9ec590f63d86f4cca13f950
KF Chan <31157514+ckf42@users.noreply.github.com>
2024-12-12 11:18:22 +0800

    update 2024 sol 11
    using idea from https://redd.it/1hbplc0 this speeds up 10k blinks to
    ~11s

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,6 +1,8 @@
 import AOCInit
 import util
 
+from functools import cache
+
 if __name__ != '__main__':
     exit()
 
@@ -8,54 +10,43 @@ inp = util.getInput(d=11, y=2024)
 
 nums = util.getInts(inp)
 
-cache: dict[tuple[int, int], int] = dict()
-
-def tryEval(buff: list[tuple[int, int]]) -> None:
-    while len(buff) != 0:
-        x, count = buff[-1]
-        if (x, count) in cache:
-            buff.pop()
-        elif count == 0:
-            cache[(x, 0)] = 1
-            buff.pop()
-        elif x == 0:
-            if (1, count - 1) not in cache:
-                buff.append((1, count - 1))
-            else:
-                cache[(0, count)] = cache[(1, count - 1)]
-                buff.pop()
-        elif len(s := str(x)) & 1:
-            if (x * 2024, count - 1) not in cache:
-                buff.append((x * 2024, count - 1))
-            else:
-                cache[(x, count)] = cache[(x * 2024, count - 1)]
-                buff.pop()
-        else:
-            a = int(s[:len(s) // 2])
-            b = int(s[len(s) // 2:])
-            uneval = False
-            for y in (a, b):
-                if (y, count - 1) not in cache:
-                    uneval = True
-                    buff.append((y, count - 1))
-            if not uneval:
-                cache[(x, count)] = sum(cache[(y, count - 1)] for y in (a, b))
-                buff.pop()
+@cache
+def makeNewStone(x):
+    if x == 0:
+        return (1,)
+    s = str(x)
+    if len(s) & 1:
+        return (x * 2024,)
+    return (int(s[:len(s) // 2]), int(s[len(s) // 2:]))
 
 # part 1
-timer = util.Timer()
-tryEval([(x, 25) for x in nums])
-print(sum(cache[(x, 25)] for x in nums))
-timer.check()
+freqDict: dict[int, int] = dict()
+for x in nums:
+    freqDict[x] = freqDict.get(x, 0) + 1
+for _ in range(25):
+    newFreqDict: dict[int, int] = dict()
+    for x, fq in freqDict.items():
+        for y in makeNewStone(x):
+            newFreqDict[y] = newFreqDict.get(y, 0) + fq
+    freqDict = newFreqDict
+print(sum(freqDict.values()))
 
 # part 2
-tryEval([(x, 75) for x in nums])
-print(sum(cache[(x, 75)] for x in nums))
-timer.check()
+for _ in range(75 - 25):
+    newFreqDict = dict()
+    for x, fq in freqDict.items():
+        for y in makeNewStone(x):
+            newFreqDict[y] = newFreqDict.get(y, 0) + fq
+    freqDict = newFreqDict
+print(sum(freqDict.values()))
 
-for t in (100, 500, 1000, 10000):
-    tryEval([(x, t) for x in nums])
-    print(sum(cache[(x, t)] for x in nums))
-    timer.check()
-timer.stop()
+# timer = util.Timer()
+# for _ in range(10000 - 75):
+#     newFreqDict = dict()
+#     for x, fq in freqDict.items():
+#         for y in makeNewStone(x):
+#             newFreqDict[y] = newFreqDict.get(y, 0) + fq
+#     freqDict = newFreqDict
+# print(sum(freqDict.values()))
+# timer.stop()
 
