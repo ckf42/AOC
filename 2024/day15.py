5467f707c4beb680957c39d0aebb39236462fa07
KF Chan <31157514+ckf42@users.noreply.github.com>
2024-12-15 14:23:49 +0800

    add 2024 sol 15
    bad. should be able to think better

---
+++ 2024/day15.py
@@ -0,0 +1,122 @@
+import AOCInit
+import util
+
+from collections import deque
+
+if __name__ != '__main__':
+    exit()
+
+inp = util.getInput(d=15, y=2024)
+
+warehouseLines = inp.split('\n\n')[0]
+warehouse = list(list(r) for r in warehouseLines.splitlines())
+n = len(warehouse)
+m = len(warehouse[0])
+inst = ''.join(r.strip() for r in inp.split('\n\n')[1])
+dirs = {
+        '^': (-1, 0),
+        '>': (0, 1),
+        'v': (1, 0),
+        '<': (0, -1),
+}
+
+# part 1
+
+x, y = divmod(warehouseLines.index('@'), m + 1)
+
+for c in inst:
+    d = dirs[c]
+    moveStack = []
+    xx, yy = x, y
+    while True:
+        if warehouse[xx][yy] == '.':
+            break
+        if warehouse[xx][yy] in '@O':
+            moveStack.append((xx, yy))
+            xx += d[0]
+            yy += d[1]
+        else:
+            moveStack.clear()
+            break
+    while len(moveStack) != 0:
+        pt = moveStack.pop()
+        warehouse[xx][yy], warehouse[pt[0]][pt[1]] = warehouse[pt[0]][pt[1]], warehouse[xx][yy]
+        x, y = xx, yy
+        xx, yy = pt
+gpss = 0
+for i in range(n):
+    for j in range(m):
+        if warehouse[i][j] == 'O':
+            gpss += 100 * i + j
+print(gpss)
+
+# part 2
+newWarehouse = list(
+        list(
+            x
+            for c in r
+            for x in {'#': '##', 'O': '[]', '.': '..', '@': '@.'}[c]
+        )
+        for r in warehouseLines.splitlines()
+)
+x, y = divmod(warehouseLines.index('@'), m + 1)
+y *= 2
+n = len(newWarehouse)
+m = len(newWarehouse[0])
+newWarehouse[x][y] = '.'
+
+def canMove(pt: tuple[int, int], d: tuple[int, int]) -> bool:
+    x, y = pt
+    xx = x + d[0]
+    yy = y + d[1]
+    obs = newWarehouse[xx][yy]
+    if obs == '.':
+        return True
+    if obs == '#':
+        return False
+    # box
+    if d[0] == 0:
+        # hori
+        return canMove((xx, yy), d)
+    else:
+        # vert
+        oX, oY = xx, yy
+        oY += 1 if obs == '[' else -1
+        return canMove((xx, yy), d) and canMove((oX, oY), d)
+
+for c in inst:
+    d = dirs[c]
+    if not canMove((x, y), d):
+        continue
+    toMove = deque([(x, y)])
+    moveBuff = []
+    willMove = set()
+    while len(toMove) != 0:
+        xx, yy = toMove.popleft()
+        if (xx, yy) in willMove:
+            continue
+        willMove.add((xx, yy))
+        newX, newY = xx + d[0], yy + d[1]
+        moveBuff.append(((xx, yy), (newX, newY)))
+        obs = newWarehouse[newX][newY]
+        if obs == '.':
+            continue
+        if d[0] != 0:
+            #vert
+            oX, oY = newX, newY
+            oY += 1 if obs == '[' else -1
+            toMove.append((oX, oY))
+        toMove.append((newX, newY))
+    while len(moveBuff) != 0:
+        pt1, pt2 = moveBuff.pop()
+        newWarehouse[pt1[0]][pt1[1]], newWarehouse[pt2[0]][pt2[1]] \
+            = newWarehouse[pt2[0]][pt2[1]], newWarehouse[pt1[0]][pt1[1]]
+    x += d[0]
+    y += d[1]
+gpss = 0
+for i in range(n):
+    for j in range(m):
+        if newWarehouse[i][j] == '[':
+            gpss += 100 * i + j
+print(gpss)
+
