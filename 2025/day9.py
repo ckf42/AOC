6165f64133d36e379a9e0ec707fd55efd4960bf1
KF Chan <31157514+ckf42@users.noreply.github.com>
2025-12-09 22:54:21 +0800

    update 2025 day 9
    works but horrendously slow. need a better algo

--- 2025/day9.py
+++ 2025/day9.py
@@ -5,130 +5,79 @@ from functools import cache
 if __name__ != "__main__":
     exit()
 
-inp = """\
-7,1
-11,1
-11,7
-9,7
-9,5
-2,5
-2,3
-7,3
-"""
-
 inp = util.getInput(d=9, y=2025)
 
 pts = [util.getInts(line) for line in inp.splitlines()]
-n = len(pts)
+k = len(pts)
 
 
 # part 1
-@cache
 def area(pt1, pt2):
-    return util.prod(abs(a - b + 1) for a, b in zip(pt1, pt2))
+    return util.prod((abs(pt1[0] - pt2[0]) + 1, abs(pt1[1] - pt2[1]) + 1))
 
 
-print(max(area(pts[i], pts[j]) for i in range(n) for j in range(i + 1, n)))
+print(max(area(pts[i], pts[j]) for i in range(k) for j in range(i + 1, k)))
 
 
 # part 2
 
-# minX, minY = min(pt[0] for pt in pts), min(pt[1] for pt in pts)
-
-# def isInside(pt: tuple[int, int]) -> bool:
-#     endPoint = (minX - 1, minY - 1)
-#     count = 0
-#     for i in range(n):
-#         res = util.segmentIntersection(
-#             *(util.Point.fromIterable(node) for node in (pt, endPoint, pts[i], pts[(i + 1) % n]))
-#         )
-#         if res is not None:
-#             # print(pt, pts[i], pts[(i + 1) % n], "intersect")
-#             count += 1
-#     # print(pt, count)
-#     return count % 2 == 1
-
-# maxArea = -1
-# for i in range(n):
-#     for j in range(i + 1, n):
-#         x, y = pts[i], pts[j]
-#         a, b = min(x[0], y[0]), min(x[1], y[1])
-#         c, d = max(x[0], y[0]), max(x[1], y[1])
-#         valid = True
-#         for xx in range(a, c + 1):
-#             for yy in range(b, d + 1):
-#                 if any(
-#                     util.pointOnSegment(
-#                         util.Point(xx, yy), util.Point.fromIterable(pts[i]), util.Point.fromIterable(pts[(i + 1) % n])
-#                     )
-#                     for i in range(n)
-#                 ):
-#                     continue
-#                 if not isInside((xx, yy)):
-#                     valid = False
-#                     # print(xx, yy, "fail")
-#                     break
-#             if not valid:
-#                 break
-#         # print(x, y, valid)
-#         if valid:
-#             maxArea = max(maxArea, area(x, y))
-# print(maxArea)
-
-xlst = [pt[0] for pt in pts]
-ylst = [pt[1] for pt in pts]
-xValCompressed = {v: i for i, v in enumerate(xlst)}
-yValCompressed = {v: i for i, v in enumerate(ylst)}
+xlst = sorted(set([pt[0] for pt in pts]))
+ylst = sorted(set([pt[1] for pt in pts]))
+xValCompressed = {v: i * 2 for i, v in enumerate(xlst)}
+yValCompressed = {v: i * 2 for i, v in enumerate(ylst)}
+n = len(xlst) * 2
+m = len(ylst) * 2
 
 compressedPts = [(xValCompressed[x], yValCompressed[y]) for x, y in pts]
 
 
 def realArea(pt1, pt2):
-    return area((xlst[pt1[0]], ylst[pt1[1]]), (xlst[pt2[0]], ylst[pt2[1]]))
+    return area((xlst[pt1[0] // 2], ylst[pt1[1] // 2]), (xlst[pt2[0] // 2], ylst[pt2[1] // 2]))
 
 
-@cache
-def isInside(pt: tuple[int, int]) -> bool:
-    endPoint = (-1, -1)
-    count = 0
-    for i in range(n):
-        if pt == compressedPts[i]:
-            return True
-        if (
-            util.segmentIntersection(
-                *(
-                    util.Point.fromIterable(node)
-                    for node in (pt, endPoint, compressedPts[i], compressedPts[(i + 1) % n])
-                )
-            )
-            is not None
-        ):
-            count += 1
-    return count % 2 == 1
+compressedGrid = [[None] * m for _ in range(n)]
+for (x1, y1), (x2, y2) in zip(compressedPts, compressedPts[1:] + [compressedPts[0]]):
+    if x1 == x2:
+        d = 0, (1 if y1 < y2 else -1)
+    else:
+        d = (1 if x1 < x2 else -1), 0
+    compressedGrid[x2][y2] = True
+    x, y = x1, y1
+    while (x, y) != (x2, y2):
+        compressedGrid[x][y] = True
+        x += d[0]
+        y += d[1]
 
-# maxArea = -1
-# for i in range(n):
-#     for j in range(i + 1, n):
-#         x, y = compressedPts[i], compressedPts[j]
-#         a, b = min(x[0], y[0]), min(x[1], y[1])
-#         c, d = max(x[0], y[0]), max(x[1], y[1])
-#         valid = True
-#         for xx in range(a, c + 1):
-#             for yy in range(b, d + 1):
-#                 if any(
-#                     util.pointOnSegment(
-#                         util.Point(xx, yy),
-#                         util.Point.fromIterable(compressedPts[idx]),
-#                         util.Point.fromIterable(compressedPts[(idx + 1) % n]),
-#                     )
-#                     for idx in range(n)
-#                 ):
-#                     continue
-#                 if not isInside((xx, yy)):
-#                     valid = False
-#                     break
-#             if not valid:
-#                 break
-#         if valid:
-#             maxArea = max(maxArea, realArea(x, y))
-# print(maxArea)
+def isInside(i, j):
+    global compressedGrid
+    if compressedGrid[i][j] is None:
+        total = 0
+        for pt1, pt2 in zip(compressedPts, compressedPts[1:] + [compressedPts[0]]):
+            insect = util.segmentIntersection(*(util.Point(a, b) for a, b in ((-1, -1), (i, j), pt1, pt2)))
+            if insect is None:
+                continue
+            else:
+                total += 1 if insect == util.Point(*pt1) or insect == util.Point(*pt2) else 2
+        compressedGrid[i][j] = (total // 2) % 2 == 1
+    return compressedGrid[i][j]
+
+maxArea = -1
+for i in range(k):
+    for j in range(i + 1, k):
+        pt1, pt2 = compressedPts[i], compressedPts[j]
+        a, b = min(pt1[0], pt2[0]), min(pt1[1], pt2[1])
+        c, d = max(pt1[0], pt2[0]), max(pt1[1], pt2[1])
+        valid = True
+        for x in range(a, c + 1):
+            if not isInside(x, b) or not isInside(x, d):
+                valid = False
+                break
+        if valid:
+            for y in range(b, d + 1):
+                if not isInside(a, y) or not isInside(c, y):
+                    valid = False
+                    break
+        if valid:
+            maxArea = max(maxArea, realArea(pt1, pt2))
+print(maxArea)
+
